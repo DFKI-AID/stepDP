d3dd9ef885f18c60b39e5f4d2847dfca348166cb
Yannick Körber <yako02@dfki.de>
2019-04-12 13:50:39 +0200

    added equals and hash to  vector classes

--- src/core/src/main/java/de/dfki/step/util/Vector3.java
+++ src/core/src/main/java/de/dfki/step/util/Vector3.java
@@ -1,5 +1,7 @@
 package de.dfki.step.util;
 
+import java.util.Objects;
+
 /**
  */
 public class Vector3 {
@@ -26,5 +28,20 @@ public class Vector3 {
         double dy = y - other.y;
         double dz = z - other.z;
         return Math.sqrt(dx*dx + dy*dy + dz*dz);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vector3 vector3 = (Vector3) o;
+        return Double.compare(vector3.x, x) == 0 &&
+                Double.compare(vector3.y, y) == 0 &&
+                Double.compare(vector3.z, z) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z);
     }
 }
