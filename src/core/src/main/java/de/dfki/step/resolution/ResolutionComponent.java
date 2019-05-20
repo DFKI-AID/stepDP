c7f06a935f202619218d43a4a3f85c87c2cba75f
Magdalena Kaiser <maka04@employee-dynamic-63-205.sb.dfki.de>
2019-05-20 10:52:19 +0200

    modified ResolutionComponent

--- src/core/src/main/java/de/dfki/step/resolution/ResolutionComponent.java
+++ src/core/src/main/java/de/dfki/step/resolution/ResolutionComponent.java
@@ -1,19 +1,16 @@
 package de.dfki.step.resolution;
 
 import de.dfki.step.core.*;
-import de.dfki.step.fusion.FusionComponent;
-import de.dfki.step.fusion.FusionNode;
-import de.dfki.step.fusion.Match;
 import de.dfki.step.kb.Attribute;
 import de.dfki.step.kb.AttributeValue;
 import de.dfki.step.kb.Entity;
+import org.pcollections.PSequence;
 import org.pcollections.PSet;
+import org.pcollections.TreePVector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.Duration;
 import java.util.*;
-import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class ResolutionComponent implements Component {
@@ -21,17 +18,17 @@ public class ResolutionComponent implements Component {
     private static Logger log = LoggerFactory.getLogger(ResolutionComponent.class);
     private ComponentManager cm;
     private Supplier<Collection<Entity>> personSupplier;
-    private Supplier<Collection<Entity>> entitySupplier;
+    private Supplier<Collection<Entity>> physicalEntitySupplier;
     private Supplier<Collection<Entity>> sessionSupplier;
-    private List<ReferenceDistribution> distributions = new ArrayList<>();
+    private PSequence<ReferenceDistribution> distributions = TreePVector.empty();
 
 
     public void setPersonSupplier(Supplier<Collection<Entity>> personSupplier) {
         this.personSupplier = personSupplier;
     }
 
-    public void setEntitySupplier(Supplier<Collection<Entity>> entitySupplier) {
-        this.entitySupplier = entitySupplier;
+    public void setPhysicalEntitySupplier(Supplier<Collection<Entity>> physicalEntitySupplier) {
+        this.physicalEntitySupplier = physicalEntitySupplier;
     }
 
     public void setSessionSupplier(Supplier<Collection<Entity>> sessionSupplier) {
@@ -48,7 +45,7 @@ public class ResolutionComponent implements Component {
 
     }
 
-    private List<ReferenceDistribution> getReferenceDistribution() {
+    public PSequence<ReferenceDistribution> getReferenceDistribution() {
         return distributions;
     }
 
@@ -56,7 +53,7 @@ public class ResolutionComponent implements Component {
     public void update() {
         PSet<Token> tokens = cm.retrieveComponent(InputComponent.class).getTokens();
         for(Token token: tokens) {
-            distributions.add(doResolution(token));
+            distributions.plus(doResolution(token));
         }
     }
 
@@ -66,35 +63,38 @@ public class ResolutionComponent implements Component {
         ReferenceResolver rr = null;
         ReferenceDistribution distribution = new ReferenceDistribution();
 
-        if(token.has("slot_info")) {
-            Map<String, Object> slotinfo = (Map<String,Object>) token.get("slot_info").get();
-            if(slotinfo.containsKey("slot_type")) {
-                if(slotinfo.get("slot_type").equals("entity")) {
-                    if(slotinfo.get("entity_type").equals("person")) {
-                        rr = new PersonRR(personSupplier);
-                    }else if(slotinfo.get("entity_type").equals("personal_pronoun")) {
-                        rr = new PersonPronounRR(personSupplier, sessionSupplier);
-                    }else {
-                        rr = new ObjectRR(entitySupplier);
-                        if(slotinfo.containsKey("personal_pronoun")) {
-                            ((ObjectRR) rr).setPronoun((String)slotinfo.get("personal_pronoun"));
-                        }
-                        if(slotinfo.containsKey("attributes")) {
-                            Map<Attribute, AttributeValue> attrMap = new HashMap<>();
-                            List<Map<String, Object>> attributes = (List) slotinfo.get("attributes");
-                            for(Map<String, Object> attr: attributes) {
-                                Attribute a = new Attribute((String)attr.get("attribute_type"));
-                                String aValue = (String) attr.get("attribute_value");
-                                attrMap.put(a, new AttributeValue(aValue, aValue, a));
+        if(token.has("slots")) {
+            List<Map<String,Object>> slots = (List<Map<String, Object>>) token.get("slots").get();
+            for(Map<String, Object> slotinfo: slots) {
+                if(slotinfo.containsKey("slot_type")) {
+                    if(slotinfo.get("slot_type").equals("entity")) {
+                        if(slotinfo.get("entity_type").equals("person")) {
+                            rr = new PersonRR(personSupplier);
+                        }else if(slotinfo.get("entity_type").equals("personal_pronoun")) {
+                            rr = new PersonPronounRR(personSupplier, sessionSupplier);
+                        }else {
+                            rr = new ObjectRR(physicalEntitySupplier);
+                            if(slotinfo.containsKey("personal_pronoun")) {
+                                ((ObjectRR) rr).setPronoun((String)slotinfo.get("personal_pronoun"));
                             }
-                            ((ObjectRR) rr).setAttrMap(attrMap);
+                            if(slotinfo.containsKey("attributes")) {
+                                Map<Attribute, AttributeValue> attrMap = new HashMap<>();
+                                List<Map<String, Object>> attributes = (List) slotinfo.get("attributes");
+                                for(Map<String, Object> attr: attributes) {
+                                    Attribute a = new Attribute((String)attr.get("attribute_type"));
+                                    String aValue = (String) attr.get("attribute_value");
+                                    attrMap.put(a, new AttributeValue(aValue, aValue, a));
+                                }
+                                ((ObjectRR) rr).setAttrMap(attrMap);
 
+                            }
                         }
+                    }else if(slotinfo.get("slot_type").equals("location")) {
+                        //TODO
                     }
-                }else if(slotinfo.get("slot_type").equals("location")) {
-                    //TODO
                 }
             }
+
 
         }
         if(rr != null) {
