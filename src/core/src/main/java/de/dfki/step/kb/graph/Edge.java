bca6b7ae00b96356830aad25066c91a036007f6e
Rashid Ahmed <37401217+Rashid-Ahmed@users.noreply.github.com>
2022-07-11 08:34:46 +0200

    Standardized variable and method names

--- src/core/src/main/java/de/dfki/step/kb/graph/Edge.java
+++ src/core/src/main/java/de/dfki/step/kb/graph/Edge.java
@@ -1,22 +1,21 @@
-package de.dfki.step.kb.Graph_KB;
-import com.google.gson.Gson;
-import de.dfki.step.kb.IKBObject;
+package de.dfki.step.kb.graph;
 
 import java.util.UUID;
 
 public class Edge {
 
-    public UUID Edge_UUID;
-    public UUID child_UUID;
-    public UUID parent_UUID;
-    public String label;
+    public UUID _edgeUUID;
+    public UUID _childUUID;
+    public UUID _parentUUID;
+    public String _label;
 
     public Edge(UUID PreviousUUID, UUID NextUUID, String Edge_label)
     {
-        this.child_UUID = PreviousUUID;
-        this.parent_UUID = NextUUID;
-        this.label = Edge_label;
-        this.Edge_UUID = UUID.randomUUID();
+        this._childUUID = PreviousUUID;
+        this._parentUUID = NextUUID;
+        this._label = Edge_label;
+        this._edgeUUID = UUID.randomUUID();
+
     }
 
 }
